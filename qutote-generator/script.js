79cbc2653ea557877df3cbee713cbca17a00744e
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-12 12:51:12 -0300

    Manipulating DOM With JavaScript

--- qutote-generator/script.js
+++ qutote-generator/script.js
@@ -1,10 +1,30 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = []
 
 //show new quote
 function newQuote() {
     // pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    console.log(quote)
+    // check if author field is bland and replace it with 'unknown'
+
+    if(!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.text
+    }
+    // check quote length to determine styling
+    if(quote.text.length > 120) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent = quote.text
 }
 
 // get quotes from API
@@ -18,6 +38,17 @@ async function getQuotes() {
         //catch error here
     }
 }
+
+// tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(twitterUrl, '_black')
+
+}
+
+// event listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
 //on load
 getQuotes()
