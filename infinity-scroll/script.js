caa8b49c002ba5681aa92c033644fe88398a2331
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-13 10:53:36 -0300

    JavaScript To Display Data

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -1,4 +1,38 @@
+const imageContainer = document.getElementById('image-container')
+const loader = document.getElementById('loader')
 
+let photosArray = []
+
+// Helper func to set attributes on DOM elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
+// Create Elements for Links & Photos, add to DOM
+function displayPhotos() {
+    // Run func for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Create <a> to link to unsplash
+        const item = document.createElement('a')
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_black',
+        })
+        // Create <img> for photo
+        const img = document.createElement('img')
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+        // Put <img> inside <a>, then puth both inside imageContainer Element
+        item.appendChild(img)
+        imageContainer.appendChild(item)
+
+    })
+}
 
 // Unsplash API
 const count = 10
@@ -9,8 +43,8 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl)
-        const data = await response.json()
-        console.log(data)
+        photosArray = await response.json()
+        displayPhotos()
     } catch (error) {
         //Catch Error Here
     }
