b1cc3902f5b06c84350d7c8c762a98c6b34ef2cc
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-13 15:16:18 -0300

    update

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -5,9 +5,10 @@ let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
 let photosArray = [];
+let initialLoad = true
 
 // Unsplash API
-const count = 30;
+let count = 5;
 const apiKey = 'EZLWfRjozEQMnYuyz8jIFlKyG-RRVp78jTxQTLCJteY';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
@@ -17,6 +18,8 @@ function imageLoaded() {
   if (imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
+    initialLoad = false
+    count = 30
   }
 }
 
