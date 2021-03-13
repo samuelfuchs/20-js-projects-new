fbe0f12fb244dd84f3241c1cf12227c58d87ca31
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-13 14:52:01 -0300

    Infinite Scroll With JavaScript

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -50,5 +50,12 @@ async function getPhotos() {
     }
 }
 
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+        getPhotos()
+    }
+})
+
 // On load
 getPhotos()
