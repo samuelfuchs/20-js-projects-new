8d7f8bddfc41594df66d815433ffee43eb98609f
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-14 21:24:42 -0300

    LocalStorage To Save User Data

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -37,12 +37,25 @@ function lightMode() {
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
+        localStorage.setItem('theme', 'dark')
         darkMode()
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
+        localStorage.setItem('theme', 'light')
         lightMode()
     }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme)
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme')
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme)
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true
+        darkMode()
+    }
+}
