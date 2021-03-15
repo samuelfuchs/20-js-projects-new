ab71939add826949e9033ebd1d51317d498a7fd8
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-14 21:18:58 -0300

    Toggle Theme With JavaScript 2

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -1,12 +1,46 @@
 // Select specific item with "[]"
 const toggleSwitch = document.querySelector('input[type="checkbox"]')
 
+// Selection of other elements to toggle to dark theme
+const nav = document.getElementById('nav')
+const toggleIcon = document.getElementById('toggle-icon')
+const image1 = document.getElementById('image2')
+const image2 = document.getElementById('image2')
+const image3 = document.getElementById('image3')
+const textBox = document.getElementById('text-box')
+
+// Dark or Light Images
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`
+    image2.src = `img/undraw_feeling_proud_${color}.svg`
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`
+}
+// Dark Mode Styles
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)'
+    toggleIcon.children[0].textContent = 'Dark Mode'
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+    imageMode('dark')
+}
+
+// Light Mode Styles
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)'
+    toggleIcon.children[0].textContent = 'Light Mode'
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
+    imageMode('light')
+}
+
 // Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
+        darkMode()
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
+        lightMode()
     }
 }
 
