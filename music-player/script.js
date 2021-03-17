8b956847049ef51cabbb23e59b8e4a52b874613b
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-16 21:25:31 -0300

    Progress Bar

--- music-player/script.js
+++ music-player/script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img')
 const title = document.getElementById('title')
 const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
+const progressContainer = document.getElementById('progress-container')
+const progress = document.getElementById('progress')
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
@@ -87,6 +89,18 @@ function prevSong() {
 // On Load - Select First Song
 loadSong(songs[songIndex])
 
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const { duration, currentTime } = e.srcElement
+        console.log(duration, currentTime)
+        // Update Progress Bar Width
+        const progressPercent = (currentTime / duration) * 100
+        progress.style.width = `${progressPercent}%`
+    }
+}
+
 // Event Listeners
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+music.addEventListener('timeupdate', updateProgressBar)
