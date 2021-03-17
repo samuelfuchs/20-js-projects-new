ad15164a7db51f538bba43b7b2461699edb353aa
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-16 21:12:34 -0300

    Music Player With JavaScript

--- music-player/script.js
+++ music-player/script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
-const nextBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+
+// Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Fron Row (Remix)',
+        artist: 'Metric/Jacinto Design',
+    }
+]
 
 // Check if Playing
 let isPlaying = false
@@ -24,3 +51,44 @@ function pauseSong() {
 
 // Play or Pause EventListener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()))
+
+// Update DOM
+function loadSong (song) {
+    title.textContent = song.displayName
+    artist.textContent = song.artist
+    music.src = `music/${song.name}.mp3`
+    image.src = `img/${song.name}.jpg`
+}
+
+// Current Song
+let songIndex = 0
+
+// Next Song
+function nextSong() {
+    songIndex++
+    if (songIndex > songs.length -1) {
+        songIndex = 0
+    }
+    console.log(songIndex)
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+// Previous Song
+function prevSong() {
+    songIndex--
+    if (songIndex < 0) {
+        songIndex = songs.length -1
+    }
+    console.log(songIndex)
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+
+// On Load - Select First Song
+loadSong(songs[songIndex])
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
