460da0a3a5865351aa6fb0c052fd7bd0da5d4c07
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-18 20:59:21 -0300

    JS - Countdown Page

--- -- template-math-sprint-game/script.js
+++ -- template-math-sprint-game/script.js
@@ -82,6 +82,26 @@ function createEquations() {
 //   bottomSpacer.classList.add('height-500');
 //   itemContainer.appendChild(bottomSpacer);
 // }
+// Displays 3, 2, 1, Go!
+function countdownStart() {
+  countdown.textContent = '3'
+  setTimeout(() => {
+    countdown.textContent = '2'
+  }, 1000)
+  setTimeout(() => {
+    countdown.textContent = '1'
+  }, 2000)
+  setTimeout(() => {
+    countdown.textContent = 'GO!'
+  }, 3000)
+}
+
+// Navigate from Splash Page to Countdown Page
+function showCountdown() {
+  countdownPage.hidden = false
+  splashPage.hidden = true
+  countdownStart()
+}
 
 // Get the value from selected radio button
 function getRadioValue() {
@@ -99,6 +119,9 @@ function selectQuestionAmount(e) {
   e.preventDefault()
   questionAmount = getRadioValue()
   console.log('question amount:', questionAmount)
+  if (questionAmount) {
+    showCountdown()
+  }
 }
 
 startForm.addEventListener('click', () =>{
