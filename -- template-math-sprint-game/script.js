db0f044c47751fa47c01427bc37e5c7c1fc0c473
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-18 20:53:01 -0300

    JS - Radio Selection & Functionality

--- -- template-math-sprint-game/script.js
+++ -- template-math-sprint-game/script.js
@@ -19,7 +19,7 @@ const penaltyTimeEl = document.querySelector('.penalty-time');
 const playAgainBtn = document.querySelector('.play-again');
 
 // Equations
-
+let questionAmount = 0
 let equationsArray = [];
 
 // Game Page
@@ -82,3 +82,35 @@ function createEquations() {
 //   bottomSpacer.classList.add('height-500');
 //   itemContainer.appendChild(bottomSpacer);
 // }
+
+// Get the value from selected radio button
+function getRadioValue() {
+  let radioValue
+  radioInputs.forEach((radioInput) => {
+    if (radioInput.checked) {
+      radioValue = radioInput.value
+    }
+  })
+  return radioValue
+}
+
+// Form that decides amount of questions
+function selectQuestionAmount(e) {
+  e.preventDefault()
+  questionAmount = getRadioValue()
+  console.log('question amount:', questionAmount)
+}
+
+startForm.addEventListener('click', () =>{
+  radioContainers.forEach((radioEl) => {
+    // Remove Selected Label Styling
+    radioEl.classList.remove('selected-label')
+    // Add it back if radio input is checked
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add('selected-label')
+    }
+  })
+})
+
+// Event Listeners
+startForm.addEventListener('submit', selectQuestionAmount)
