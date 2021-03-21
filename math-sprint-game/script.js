96c1bcb56b145b80aa23b77266899886f66bccaa
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-21 10:02:37 -0300

    JS - Score Page final

--- math-sprint-game/script.js
+++ math-sprint-game/script.js
@@ -40,17 +40,37 @@ let finalTimeDisplay = '0.0s'
 // Scroll
 let valueY = 0
 
-// Show Score Page
+// Reset Game
+function playAgain() {
+  gamePage.addEventListener('click', startTimer)
+  scorePage.hidden = true
+  splashPage.hidden = false
+  equationsArray = []
+  playerGuessArray = []
+  valueY = 0
+  playAgainBtn.hidden = true
+}
 
+// Show Score Page
+function showScorePage() {
+  // Show Play Again button after 1 second
+  setTimeout(() => {
+    playAgainBtn.hidden = false
+  }, 1000)
+  gamePage.hidden = true
+  scorePage.hidden = false
+}
 
 // Format & Display Time in DOM
 function scoresToDOM() {
   finalTimeDisplay = finalTime.toFixed(1)
   baseTime = timePlayed.toFixed(1)
   penaltyTime = penaltyTime.toFixed(1)
-  baseTimeEl.textContent = `Base Time: ${baseTime}`
+  baseTimeEl.textContent = `Base Time: ${baseTime}s`
   penaltyTimeEl.textContent = `Penalty: +${penaltyTime}s`
   finalTimeEl.textContent = `${finalTimeDisplay}s`
+  // Scroll to Top, go to Score Page
+  itemContainer.scrollTo({ top: 0, bahavior: 'instant' })
   showScorePage()
 }
 
