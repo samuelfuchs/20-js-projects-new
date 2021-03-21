fe9c5f1b8c1cf73c3c8bd36706adf2dacb2d4d13
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-21 09:41:42 -0300

    JS - Check Score with Timing Events part 2

--- math-sprint-game/script.js
+++ math-sprint-game/script.js
@@ -40,17 +40,33 @@ let finalTimeDisplay = '0.0s'
 // Scroll
 let valueY = 0
 
+// Stop Timer, Process Results, go to Score Page
+function checkTime() {
+  console.log(timePlayed)
+  if (playerGuessArray.length == questionAmount) {
+    console.log('player guess array:', playerGuessArray)
+    clearInterval(timer)
+  }
+}
+
+// Add a tenth of a second to timePlayed
+function addTime() {
+  timePlayed += 0.1
+  checkTime()
+}
+
 // Start timer when game page is clicked
 function startTimer() {
   // Reset times
   timePlayed = 0
   penaltyTime = 0
   finalTime = 0
+  timer = setInterval(addTime, 100)
+  gamePage.removeEventListener('click', startTimer)
 }
 
 // Scroll, Store user selection in playerGuessArray
 function select(guessedTrue) {
-  console.log('player guess array:', playerGuessArray)
   // Scroll 80 pixels
   valueY += 80
   itemContainer.scroll(0, valueY)
