6e14c6f9476e4814eb07d8621777071c4daf167f
Samuel Fuchs <fuchsgerson@gmail.com>
2021-03-22 03:37:47 -0300

    JS - Best Scores in Local Storage part 2

--- math-sprint-game/script.js
+++ math-sprint-game/script.js
@@ -36,10 +36,18 @@ let timePlayed = 0
 let baseTime = 0
 let penaltyTime = 0
 let finalTime = 0
-let finalTimeDisplay = '0.0s'
+let finalTimeDisplay = '0.0'
 
 // Scroll
 let valueY = 0
+
+// Refresh Splash Page Best Scores
+function bestScoresToDOM() {
+  bestScores.forEach((bestScore, index) => {
+    const bestScoreEl = bestScore
+    bestScoreEl.textContent = `${bestScoreArray[index].bestScore}s`
+  })
+}
 
 // Check Local Storage for Best Scores, set bestScoreArray
 function getSavedBestScores() {
@@ -66,6 +74,20 @@ function getSavedBestScores() {
     ]
     localStorage.setItem('bestScores', JSON.stringify(bestScoreArray))
   }
+  bestScoresToDOM()
+}
+
+// Update Best Score Array
+function updateBestScore() {
+  bestScoreArray.forEach((score, index) => {
+    // Select correct Best Score to update
+    if (questionAmount == score.questions) {
+      // Return Best Score as number with one decimal
+      const savedBestScore = Number(bestScoreArray[index].bestScore)
+      // Update if the new final score is less or replacing zero
+      if ( savedBestScore)
+    }
+  })
 }
 
 // Reset Game
